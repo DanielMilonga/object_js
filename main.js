7d63ccfe8ae0bb6f787efe5d7bbbe079870da88f
DanielMilonga <daniil.milonga@gmail.com>
2021-07-02 15:40:00 +0300

    Second Lesson

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-'ldasdlasdoakd
+const person1 = {
+    name: 'Chris',
+    greeting: function() {
+        alert('Hi! I\'m ' + this.name + '.');
+    }
+}
+
+const person2 = {
+    name: 'Brian',
+    greeting: function() {
+        alert('Hi! I\'m ' + this.name + '.');
+    }
+}
+
+person1.greeting();
